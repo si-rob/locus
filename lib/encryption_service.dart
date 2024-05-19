618b7c4c9be3278e0f99d15ba315925e5e2c199f
Simon Roberts <simon@sirob.dev>
2024-05-19 13:54:29 -0500

    update the encryption to use dotenv

--- lib/encryption_service.dart
+++ lib/encryption_service.dart
@@ -1,18 +1,21 @@
+// lib/encryption_service.dart
+
 import 'package:encrypt/encrypt.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class EncryptionService {
   final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
-  final String _keyStorageKey = 'encryption_key';
-  final String _ivStorageKey = 'encryption_iv';
+  final String _keyStorageKey = dotenv.env['KEY_STORAGE_KEY']!;
+  final String _ivStorageKey = dotenv.env['IV_STORAGE_KEY']!;
 
   Future<void> _initializeKeys() async {
     String? keyString = await _secureStorage.read(key: _keyStorageKey);
     String? ivString = await _secureStorage.read(key: _ivStorageKey);
 
     if (keyString == null || ivString == null) {
-      final key = Key.fromSecureRandom(32);
-      final iv = IV.fromSecureRandom(16);
+      final key = Key.fromSecureRandom(32); // Generate a 256-bit key
+      final iv = IV.fromSecureRandom(16);   // Generate a 128-bit IV
       await _secureStorage.write(key: _keyStorageKey, value: key.base64);
       await _secureStorage.write(key: _ivStorageKey, value: iv.base64);
     }
@@ -22,13 +25,13 @@ class EncryptionService {
     await _initializeKeys();
     final keyString = await _secureStorage.read(key: _keyStorageKey);
     final key = Key.fromBase64(keyString!);
-    return Encrypter(AES(key, mode: AESMode.cbc));
+    return Encrypter(AES(key, mode: AESMode.cbc)); // Use CBC mode for AES
   }
 
   Future<String> encryptText(String text) async {
     final encrypter = await _getEncrypter();
     final ivString = await _secureStorage.read(key: _ivStorageKey);
-    final iv = IV.fromBase64(ivString!); // Ensure iv is used
+    final iv = IV.fromBase64(ivString!); // Use the IV
     final encrypted = encrypter.encrypt(text, iv: iv);
     return encrypted.base64;
   }
@@ -36,7 +39,7 @@ class EncryptionService {
   Future<String> decryptText(String encryptedText) async {
     final encrypter = await _getEncrypter();
     final ivString = await _secureStorage.read(key: _ivStorageKey);
-    final iv = IV.fromBase64(ivString!); // Ensure iv is used
+    final iv = IV.fromBase64(ivString!); // Use the IV
     final encrypted = Encrypted.fromBase64(encryptedText);
     final decrypted = encrypter.decrypt(encrypted, iv: iv);
     return decrypted;
