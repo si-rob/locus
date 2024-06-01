d8cbd4d24e5397e14f1a1040402886ea43e3bee6
Simon Roberts <simon@sirob.dev>
2024-06-01 16:41:54 -0500

    logging on encryption - we'll need to address the root problem of losing keys on app reinstallation separately

--- lib/encryption_service.dart
+++ lib/encryption_service.dart
@@ -1,8 +1,7 @@
-// lib/encryption_service.dart
-
-import 'package:encrypt/encrypt.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/foundation.dart' as foundation;
 
 class EncryptionService {
   final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
@@ -14,24 +13,28 @@ class EncryptionService {
     String? ivString = await _secureStorage.read(key: _ivStorageKey);
 
     if (keyString == null || ivString == null) {
-      final key = Key.fromSecureRandom(32); // Generate a 256-bit key
-      final iv = IV.fromSecureRandom(16);   // Generate a 128-bit IV
+      if (foundation.kDebugMode) {
+        // ignore: avoid_print
+        print("Keys are missing, likely due to app reinstallation. Regenerating keys...");
+      }
+      final key = encrypt.Key.fromSecureRandom(32); // Generate a 256-bit key
+      final iv = encrypt.IV.fromSecureRandom(16);   // Generate a 128-bit IV
       await _secureStorage.write(key: _keyStorageKey, value: key.base64);
       await _secureStorage.write(key: _ivStorageKey, value: iv.base64);
     }
   }
 
-  Future<Encrypter> _getEncrypter() async {
+  Future<encrypt.Encrypter> _getEncrypter() async {
     await _initializeKeys();
     final keyString = await _secureStorage.read(key: _keyStorageKey);
-    final key = Key.fromBase64(keyString!);
-    return Encrypter(AES(key, mode: AESMode.cbc)); // Use CBC mode for AES
+    final key = encrypt.Key.fromBase64(keyString!);
+    return encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.cbc)); // Use CBC mode for AES
   }
 
   Future<String> encryptText(String text) async {
     final encrypter = await _getEncrypter();
     final ivString = await _secureStorage.read(key: _ivStorageKey);
-    final iv = IV.fromBase64(ivString!); // Use the IV
+    final iv = encrypt.IV.fromBase64(ivString!); // Use the IV
     final encrypted = encrypter.encrypt(text, iv: iv);
     return encrypted.base64;
   }
@@ -39,8 +42,8 @@ class EncryptionService {
   Future<String> decryptText(String encryptedText) async {
     final encrypter = await _getEncrypter();
     final ivString = await _secureStorage.read(key: _ivStorageKey);
-    final iv = IV.fromBase64(ivString!); // Use the IV
-    final encrypted = Encrypted.fromBase64(encryptedText);
+    final iv = encrypt.IV.fromBase64(ivString!); // Use the IV
+    final encrypted = encrypt.Encrypted.fromBase64(encryptedText);
     final decrypted = encrypter.decrypt(encrypted, iv: iv);
     return decrypted;
   }
